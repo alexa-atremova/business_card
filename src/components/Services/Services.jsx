61b90798a3852944bcf6f984ca60582ee1c85015
alexa-atremova <77694102+alexa-atremova@users.noreply.github.com>
2023-05-02 21:27:30 +0300

    servicws ru version added

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -17,7 +17,7 @@ const ServicesContainer = styled.div`
 `;
 
 const Title = styled.h2`
-  font-family: "Poppins";
+  font-family: "Poppins", sans-serif;
   font-size: 42px;
   font-weight: bold;
   border-bottom: 6px solid #ff6600;
@@ -33,7 +33,7 @@ const Title = styled.h2`
 `;
 
 const Description = styled.p`
-  font-family: "Poppins";
+  font-family: "Poppins", sans-serif;
   max-width: 600px;
   font-size: 16px;
   line-height: 1.7;
@@ -79,9 +79,9 @@ const BlocksContainer = styled.div`
 `;
 
 const Block = styled.div`
-  width: 250px;
+  width: 280px;
   height: 260px;
-  font-family: "Poppins";
+  font-family: "Poppins", sans-serif;
   background-color: white;
   box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.2);
   border-radius: 8px;
@@ -124,80 +124,108 @@ const BlockText = styled.p`
   }
 `;
 
-const Services = () => {
+const Services = ({ lang }) => {
   return (
     <ServicesContainer>
-      <Title>Services</Title>
+      <Title>{lang === "ru" ? "Услуги" : "Services"}</Title>
+
       <Description>
         <b>
-          {" "}
-          I offer a range of services that are tailored to your unique needs.
+          {lang === "ru"
+            ? "Я предлагаю ряд услуг, которые настроены на  Ваши уникальные потребности. "
+            : "I offer a range of services that are tailored to your unique needs."}
         </b>
-        Whether you are dealing with difficult decisions, facing a challenging
-        situation, or simply feeling overwhelmed,<b> I am here to help.</b>
+        {lang === "ru"
+          ? "Независимо от того, сталкиваетесь ли Вы с необходимостью принятия  непростых решений, или с ситуацией которая бросает Вам вызов, или просто чувствуете себя перегруженным,"
+          : "Whether you are dealing with difficult decisions, facing a challenging situation, or simply feeling overwhelmed,"}
+        <b>
+          {lang === "ru"
+            ? " я здесь, и сделаю все что в моих силах чтобы Вам помочь."
+            : " I am here to help."}
+        </b>
       </Description>
       <BlocksContainer>
         <Block>
-          <BlockTitle>Listening and providing empathy</BlockTitle>
+          <BlockTitle>
+            {lang === "ru"
+              ? "Выслушивание и эмпатия"
+              : "Listening and providing empathy"}
+          </BlockTitle>
           <BlockText>
-            I understand that sometimes what you need most is someone to listen
-            and understand. My compassionate and non-judgmental approach ensures
-            that you have a safe space to share your deepest thoughts and
-            emotions.
-          </BlockText>
-        </Block>
-        <Block>
-          <BlockTitle>Offering guidance and advice</BlockTitle>
-          <BlockText>
-            I believe in empowering my clients to take control of their lives
-            and make positive changes. With my guidance and advice, I can help
-            you identify and overcome obstacles to reach your goals and live a
-            fulfilling life.
+            {lang === "ru"
+              ? "Я понимаю, что иногда Вам больше всего нужен кто-то, кто послушает и поймет. Мой сочувствующий и непредвзятый подход обеспечит Вам безопасное пространство, чтобы Вы смогли поделиться своими самыми глубокими мыслями и эмоциями."
+              : "I understand that sometimes what you need most is someone to listen and understand. My compassionate and non-judgmental approach ensures that you have a safe space to share your deepest thoughts and emotions."}
           </BlockText>
         </Block>
         <Block>
           <BlockTitle>
-            Helping clients navigate difficult situations and decisions
+            {lang === "ru"
+              ? "Предоставление руководства и советов"
+              : "Offering guidance and advice"}
           </BlockTitle>
           <BlockText>
-            Whether you're facing a life transition or dealing with a
-            challenging situation, I am here to support you. I can help you
-            navigate difficult decisions and provide you with the tools and
-            resources you need to move forward.
+            {lang === "ru"
+              ? "Я здесь, для того чтобы дать своим клиентам возможность контролировать свою жизнь и вносить позитивные изменения. С моим руководством и советами я могу помочь Вам определить и преодолеть препятствия, для того чтобы Вы смогли достичь своих целей и жить полной жизнью."
+              : "I believe in empowering my clients to take control of their lives and make positive changes. With my guidance and advice, I can help you identify and overcome obstacles to reach your goals and live a fulfilling life."}
           </BlockText>
         </Block>
         <Block>
           <BlockTitle>
-            Providing resources and referrals for additional support
+            {lang === "ru"
+              ? "Помощь клиентам в навигации трудных ситуаций и принятии решений"
+              : "Helping clients navigate difficult situations and decisions"}
           </BlockTitle>
           <BlockText>
-            I understand that sometimes additional support may be needed beyond
-            my services. I can provide you with the necessary resources and
-            referrals to help you get the support you need.
+            {lang === "ru"
+              ? "Независимо от того, сталкиваетесь ли Вы с новой жизненной перспективой или сталкиваетесь с критической ситуацией , я здесь, чтобы поддержать Вас. Я могу помочь Вам пройти ту часть пути, которая требует принятия трудных решений и предоставить вам инструменты и ресурсы, необходимые для продвижения вперед."
+              : "Whether you're facing a life transition or dealing with a challenging situation, I am here to support you. I can help you navigate difficult decisions and provide you with the tools and resources you need to move forward."}
           </BlockText>
         </Block>
         <Block>
           <BlockTitle>
-            Specialising in relationship advice and career counseling
+            {lang === "ru"
+              ? "Предоставление ресурсов и ссылок на дополнительную поддержку"
+              : "Providing resources and referrals for additional support"}
           </BlockTitle>
           <BlockText>
-            I specialize in providing relationship advice and career counseling.
-            My experience and expertise can help you improve your relationships
-            and find the career path that is right for you.
+            {lang === "ru"
+              ? "Я понимаю, что иногда может потребоваться дополнительная поддержка, кроме моих услуг. Я могу предоставить Вам необходимые ресурсы и рекомендации , и помочь Вам получить необходимую поддержку."
+              : "I understand that sometimes additional support may be needed beyond my services. I can provide you with the necessary resources and referrals to help you get the support you need."}
           </BlockText>
         </Block>
         <Block>
           <BlockTitle>
-            Providing personalised consulting services for businesses and
-            organizations
+            {lang === "ru"
+              ? "Консультации по вопросам связанным с личными отношениями и карьерой"
+              : "Specialising in relationship advice and career counseling"}
           </BlockTitle>
           <BlockText>
-            by offering fresh perspectives and innovative solutions.
+            {lang === "ru"
+              ? "Я специализируюсь на предоставлении консультаций по вопросам отношений и карьерного консультирования. Мой опыт и экспертиза могут помочь Вам улучшить ваши личные отношения и найти карьерный путь, который подходит именно Вам."
+              : "I specialize in providing relationship advice and career counseling. My experience and expertise can help you improve your relationships and find the career path that is right for you."}
+          </BlockText>
+        </Block>
+        <Block>
+          <BlockTitle>
+            {lang === "ru"
+              ? "Предоставление индивидуальных консультационных услуг для бизнеса и организаций"
+              : "Providing personalised consulting services for businesses and organizations"}
+          </BlockTitle>
+          <BlockText>
+            {lang === "ru"
+              ? "Качественные изменения деятельности Вашего бизнеса с помощью предложения новых возможностей и инновационных решений это моя специализация."
+              : "by offering fresh perspectives and innovative solutions."}
           </BlockText>
         </Block>
         <Description>
-          At <b>CONFIDANT SERVICE</b> I am committed to helping you.{" "}
-          <p> I am here to support</p> you every step of the way.
+          {lang === "ru"
+            ? "CONFIDANT SERVICE нацелен на то, чтобы помочь Вам. "
+            : "At CONFIDANT SERVICE I am committed to helping you."}{" "}
+          <p>
+            {lang === "ru"
+              ? "И я здесь, для того чтобы поддерживать Вас на каждом шагу."
+              : "I am here to support  you every step of the way."}{" "}
+          </p>
         </Description>
       </BlocksContainer>
     </ServicesContainer>
